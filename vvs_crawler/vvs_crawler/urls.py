f1888091ebf47c34d6344937aa93972cc69c8e39
Fritz Otlinghaus <fritz@otlinghaus.it>
2016-10-20 23:26:18 +0200

    added json api

--- vvs_crawler/vvs_crawler/urls.py
+++ vvs_crawler/vvs_crawler/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from vvs_map.views import VVSDataViewSet
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'vvs_crawler.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-)
+
+]
+
+router = DefaultRouter()
+router.register(r'users', VVSDataViewSet)
--- vvs_crawler/vvs_map/views.py
+++ vvs_crawler/vvs_map/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .serializers import VVSDataSerializer
+from .models import VVSData
 # Create your views here.
+
+
+class VVSDataViewSet(ModelViewSet):
+    serializer_class = VVSDataSerializer
+    queryset = VVSData.objects.all()

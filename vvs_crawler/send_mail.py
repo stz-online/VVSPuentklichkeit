7137ae3d24a305b13ca83fd547ef60cd92888206
Fritz Otlinghaus <fritz@otlinghaus.it>
2016-11-05 13:24:54 +0100

    fixed sendgrid api

--- vvs_crawler/send_mail.py
+++ vvs_crawler/send_mail.py
@@ -1,18 +1,23 @@
 import subprocess
 import sendgrid
 from YamJam import yamjam
-from sendgrid.helpers.mail import Email, Content, Mail
+
 
 keys = yamjam("keys.yaml")
 
 output = subprocess.Popen(["psql", "-U","postgres","-d","vvs_crawler", "-c", "Select * FROM get_highest_n_delays('2016-10-01', '2016-10-20', 20);"],
                           stdout=subprocess.PIPE).communicate()
-sg = sendgrid.SendGridAPIClient(apikey=keys['sendgrid']['key'])
-from_email = Email(keys['vvs']['from_mail'])
 subject = "VVS Report"
-to_email = Email(keys['vvs']['email_1'])
-content = Content("text/plain", output)
-mail = Mail(from_email, subject, to_email, content)
-response = sg.client.mail.send.post(request_body=mail.get())
+client = sendgrid.SendGridClient(keys['sendgrid']['key'])
+message = sendgrid.Mail()
+
+message.add_to(keys['vvs']['email_1'])
+message.set_from(keys['vvs']['from_mail'])
+message.set_subject("VVS Report")
+message.set_html(output)
+
+client.send(message)
+
+
 
 
